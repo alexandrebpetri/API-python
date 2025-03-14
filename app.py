6b97cad379e854eb51543928d6f37a87c0781e0c
alexandrepetri <alexandrepetri.dev@gmail.com>
2025-03-14 12:00:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -25,12 +25,41 @@ livros = [
 
 #consulta todos os livros
 
-app.route('/livros')
-
+@app.route('/livros', methods=['GET'])
 def obter_livros():
     return jsonify(livros)
 
 #consulta um livro pelo id
+@app.route('/livros/<int:id>',methods=['GET'])
+def obter_livro_por_id(id):
+    for livro in livros:
+        if livro.get('id') == id:
+            return jsonify(livro)
+        
 #Editar
+@app.route('/livros/<int:id>',methods=['PUT'])
+def editar_livro_por_id(id):
+    livro_alterado = request.get_json()
+    for indice,livro in enumerate(livros):
+        if livro.get('id') == id:
+            livros[indice].update(livro_alterado)
+            return jsonify(livros[indice])
+        
+#Criar
+@app.route('/livros',methods=['POST'])
+def incluir_novo_livro():
+    novo_livro = request.get_json()
+    livros.append(novo_livro)
+    
+    return jsonify(livros)
 #Deletar
+@app.route('/livros/<int:id>',methods=['DELETE'])
+def excluir_livro(id):
+    for indice, livro in enumerate(livros):
+        if livro.get('id') == id:
+            del livros[indice]
+
+    return jsonify(livros)
+
+        
 app.run(port=5000, host='localhost', debug=True)
